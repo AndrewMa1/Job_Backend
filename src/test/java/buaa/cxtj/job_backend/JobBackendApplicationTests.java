e69d7c794e5dc493cc7616cace4ef36bc28c5bfa
Andrew_Ma <tianyangma@163.com>
2024-06-18 22:38:25 +0800

    initial commit

---
+++ src/test/java/buaa/cxtj/job_backend/JobBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package buaa.cxtj.job_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
