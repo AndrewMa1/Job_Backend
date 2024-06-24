60a7d0b64ebef0739371453b6701f67cadaf785a
Andrew_Ma <tianyangma@163.com>
2024-06-25 00:03:43 +0800

    修改Kafka Consumer

--- src/main/java/buaa/cxtj/job_backend/Controller/KafkaConsumer.java
+++ src/main/java/buaa/cxtj/job_backend/Controller/KafkaConsumer.java
@@ -2,21 +2,26 @@ package buaa.cxtj.job_backend.Controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
-@Slf4j
-@EnableKafka
 public class KafkaConsumer {
 
+    @Autowired
+    StringRedisTemplate redisTemplate;
 
-//    @KafkaListener(topics = "test", groupId = "mty")
-//    public void test(String msg) {
-//        log.info("从test分区收到消息: " + msg);
-//        stringRedisTemplate.opsForValue().set("val", "1");
-//    }
+    @KafkaListener(topics = {"test"},groupId = "")
+    public void onMessage1(ConsumerRecord<?, ?> record){
+        // 消费的哪个topic、partition的消息,打印出消息内容
+        System.out.println("简单消费Topic："+record.topic()+"**分区"+record.partition()+"**值内容"+record.value());
+        redisTemplate.opsForValue().set("val","111");
+    }
 
 }
