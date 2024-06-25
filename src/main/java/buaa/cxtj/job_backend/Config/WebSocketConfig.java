5a3666df806e1e025ef5a9a360bb04bad244a0b4
andrew-ma <tianyangma@163.com>
2024-06-25 15:40:49 +0800

    修改mysql配置

---
+++ src/main/java/buaa/cxtj/job_backend/Config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package buaa.cxtj.job_backend.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // 聊天客户端将使用此连接到服务器
+        registry.addEndpoint("/ws-chat").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic/");
+    }
+}
+
--- src/main/java/buaa/cxtj/job_backend/Controller/ChatController.java
+++ src/main/java/buaa/cxtj/job_backend/Controller/ChatController.java
@@ -6,7 +6,9 @@ import buaa.cxtj.job_backend.POJO.Entity.Message;
 import buaa.cxtj.job_backend.POJO.UserHolder;
 import buaa.cxtj.job_backend.Util.ReturnProtocol;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.*;
@@ -34,11 +36,22 @@ public class ChatController {
     @PostMapping("/sendMsg")
     public ReturnProtocol sendMessage(@RequestBody Message message) {
         message.setTimestamp(LocalDateTime.now().toString());
-        System.out.println(topic);
             //发送消息到Kafka主题队列
         kafkaTemplate.send(topic, message);
         return new ReturnProtocol(true,"发送消息成功",message);
     }
+
+
+    @KafkaListener(topics = {"chat"},groupId = "chat_cons")
+    public void onMessage(ConsumerRecord<?, ?> record){
+        // 消费的哪个topic、partition的消息,打印出消息内容
+        System.out.println("简单消费Topic："+record.topic()+"**分区"+record.partition()+"**值内容"+record.value());
+
+    }
+
+
+
+
 
 
 }
