cc9ccedd869b8fff24ae4cd34265d4ea5080a97e
小呀嘛小二郎 <2857598381@qq.com>
2024-06-25 11:51:54 +0800

    修改配置信息

---
+++ src/main/java/buaa/cxtj/job_backend/Controller/EmployController.java
@@ -0,0 +1,15 @@
+package buaa.cxtj.job_backend.Controller;
+
+import buaa.cxtj.job_backend.Util.ReturnProtocol;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/employ/")
+public class EmployController {
+
+    @GetMapping(value = "/postInfo/{id}")
+    public ReturnProtocol postJobInfo(@PathVariable String id){
+
+    }
+}
