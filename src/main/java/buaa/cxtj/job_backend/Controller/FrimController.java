efbbc5cd8773b4ddcc6706e9a51fbec76e3ab503
Zpd <747866951@qq.com>
2024-06-25 14:05:41 +0800

    创建企业1

--- src/main/java/buaa/cxtj/job_backend/Controller/FrimController.java
+++ src/main/java/buaa/cxtj/job_backend/Controller/FrimController.java
@@ -3,19 +3,21 @@ package buaa.cxtj.job_backend.Controller;
 import buaa.cxtj.job_backend.Service.FirmService;
 import buaa.cxtj.job_backend.Util.ReturnProtocol;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/firm")
 @RequiredArgsConstructor
-public class FirmController {
+public class FrimController {
     private final FirmService firmService;
 
-    @GetMapping("/createFirm")
+    @PostMapping("/createFirm")
     public ReturnProtocol createFirm(@RequestParam String id,@RequestParam String name,@RequestParam String intro,@RequestParam String picture){
         return firmService.createFirm(id,name,intro,picture);
+    }
+
+    @GetMapping("/showContent")
+    public ReturnProtocol showContent(@RequestParam String id){
+        return null;
     }
 }
