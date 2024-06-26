1ec2564bc5ec13dbd60d1962c2866370f1fd6089
Zpd <747866951@qq.com>
2024-06-26 18:25:21 +0800

    2

--- src/main/java/buaa/cxtj/job_backend/POJO/DTO/TransDTO.java
+++ src/main/java/buaa/cxtj/job_backend/POJO/DTO/TransDTO.java
@@ -2,18 +2,24 @@ package buaa.cxtj.job_backend.POJO.DTO;
 
 import lombok.Data;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Data
 public class TransDTO {
     private String transName;
     private String userId;
     private String content;
+    private String transId;
+    private String createTime;
 
     public TransDTO(String transName, String userId, String content, String transId) {
         this.transName = transName;
         this.userId = userId;
         this.content = content;
         this.transId = transId;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.createTime = sdf.format(new Date());;
     }
 
-    private String transId;
 }
