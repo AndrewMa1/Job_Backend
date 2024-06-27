6b7293cb076aa57f300151ee55041aac8d4c4322
Zpd <747866951@qq.com>
2024-06-27 12:38:05 +0800

    joker3

--- src/main/java/buaa/cxtj/job_backend/POJO/DTO/JobDTO.java
+++ src/main/java/buaa/cxtj/job_backend/POJO/DTO/JobDTO.java
@@ -18,10 +18,14 @@ public class JobDTO {
     private String internTime;
     private int bonus;
 
-    public JobDTO( String postId,String jobName, String jobRequirements, String jobCounts) {
+    public JobDTO(String postId, String jobName, String jobRequirements, String jobCounts, int wage, String workPlace, String internTime, int bonus) {
         this.postId = postId;
         this.jobName = jobName;
         this.jobRequirements = jobRequirements;
         this.jobCounts = jobCounts;
+        this.wage = wage;
+        this.workPlace = workPlace;
+        this.internTime = internTime;
+        this.bonus = bonus;
     }
 }
