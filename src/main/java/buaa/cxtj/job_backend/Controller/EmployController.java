3216b7bad0b1dd0893b7ae73ccdcf124eef1ed2f
andrew-ma <tianyangma@163.com>
2024-06-27 12:45:50 +0800

    fix search AI

--- src/main/java/buaa/cxtj/job_backend/Controller/EmployController.java
+++ src/main/java/buaa/cxtj/job_backend/Controller/EmployController.java
@@ -36,7 +36,7 @@ public class EmployController {
         return null;
     }
 
-    @GetMapping("delivery")
+    @PostMapping("delivery")
     public ReturnProtocol deliveryPost(@RequestParam String corporation_id,@RequestParam String user_id,@RequestParam String post_id,@RequestParam("file") MultipartFile file){
         try {
             log.info("基本信息为: "+corporation_id+'\n'+user_id+'\n'+post_id+'\n'+file.getOriginalFilename());
